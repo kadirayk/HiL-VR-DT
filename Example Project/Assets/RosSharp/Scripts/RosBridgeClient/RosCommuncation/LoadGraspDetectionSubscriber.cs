1f010ce253de090c771375aa9e1da2861f4eb0bf
kadirayk <kadiray.k@hotmail.com>
2020-02-25 19:17:39 +0100

    grasp detection topic subscriber

---
+++ Example Project/Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/LoadGraspDetectionSubscriber.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RosSharp.RosBridgeClient
+{
+	[RequireComponent(typeof(RosConnector))]
+	public class LoadGraspDetectionSubscriber : Subscriber<MessageTypes.Detection.DetectedObjects>
+	{
+		private bool isMessageReceived;
+		private int numberOfObjects;
+
+		protected override void Start()
+		{
+			base.Start();
+		}
+		private void Update()
+		{
+			if (isMessageReceived)
+				ProcessMessage();
+		}
+
+		protected override void ReceiveMessage(MessageTypes.Detection.DetectedObjects detectedObjects)
+		{
+			numberOfObjects = detectedObjects.objects.Length;
+			isMessageReceived = true;
+		}
+
+		private void ProcessMessage()
+		{
+			Debug.Log(numberOfObjects);
+			isMessageReceived = false;
+		}
+
+	}
+}
