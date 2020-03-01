88361cffe8c5ad698d471558eee9293acd1a4f33
kadirayk <kadiray.k@hotmail.com>
2020-03-01 15:02:46 +0100

    label visualized objects with gizmos

---
+++ Example Project/Assets/PDDL/Scripts/GizmoManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class GizmoManager : MonoBehaviour
+{
+
+	void OnDrawGizmos()
+	{
+		GameObject[] target = GameObject.FindGameObjectsWithTag("PDDLObject");
+		if (target != null)
+		{
+			for (int i = 0; i < target.Length; i++)
+			{
+				Handles.Label(target[i].transform.position, target[i].name);
+			}
+		}
+	}
+}
