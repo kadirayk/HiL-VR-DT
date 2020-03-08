c99b45132cc4d3b54b36f36de3d852a2d5fe9b2f
kadirayk <kadiray.k@hotmail.com>
2020-03-08 20:13:34 +0100

    object creation with corrected coordinates

--- Example Project/Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/LoadGraspDetectionSubscriber.cs
+++ Example Project/Assets/RosSharp/Scripts/RosBridgeClient/RosCommuncation/LoadGraspDetectionSubscriber.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Assets.PDDL;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,6 +14,7 @@ namespace RosSharp.RosBridgeClient
 		private MessageTypes.Detection.DetectedObjects detectedObj;
 		private bool shouldUpdateVisualization;
 		public GameObject cubePrefab;
+		private Dictionary<string, MessageTypes.Geometry.Point> graspPositions = new Dictionary<string, MessageTypes.Geometry.Point>();
 
 		protected override void Start()
 		{
@@ -42,23 +44,31 @@ namespace RosSharp.RosBridgeClient
 			int i = 0;
 			foreach (MessageTypes.Detection.DetectedObject obj in detectedObj.objects)
 			{
-				UnityEngine.Vector3 startPoint = TransformExtensions.Ros2Unity(new UnityEngine.Vector3((float)obj.startPoint.x, (float)obj.startPoint.y, (float)obj.startPoint.z));
-				UnityEngine.Vector3 endPoint = TransformExtensions.Ros2Unity(new UnityEngine.Vector3((float)obj.endPoint.x, (float)obj.endPoint.y, (float)obj.endPoint.z));
-				UnityEngine.Vector3 graspPoint = TransformExtensions.Ros2Unity(new UnityEngine.Vector3((float)obj.graspPoint.x, (float)obj.graspPoint.y, (float)obj.graspPoint.z));
+				//UnityEngine.Vector3 startPoint = TransformExtensions.Ros2Unity(new UnityEngine.Vector3((float)obj.startPoint.x, (float)obj.startPoint.y, (float)obj.startPoint.z));
+				//UnityEngine.Vector3 endPoint = TransformExtensions.Ros2Unity(new UnityEngine.Vector3((float)obj.endPoint.x, (float)obj.endPoint.y, (float)obj.endPoint.z));
+				MessageTypes.Geometry.Point grasp = obj.graspPoint;
+				UnityEngine.Vector3 graspPoint = new UnityEngine.Vector3((float)obj.graspPoint.x, (float)obj.graspPoint.y, (float)obj.graspPoint.z);
 				UnityEngine.Quaternion rotation = TransformExtensions.Ros2Unity(new UnityEngine.Quaternion((float)obj.rotation.x, (float)obj.rotation.y, (float)obj.rotation.z, (float)obj.rotation.w));
 				Color color = new Color(obj.color[0], obj.color[1], obj.color[2]);
 
 
 				GameObject table = GameObject.Find("Table");
-				GameObject cube = Instantiate(cubePrefab, new UnityEngine.Vector3(0.847f - graspPoint.z, graspPoint.y + table.GetComponent<Renderer>().bounds.max.y - 0.0125f, 1.18f + graspPoint.x), rotation);
+				//GameObject cube = Instantiate(cubePrefab, new UnityEngine.Vector3(0.847f - graspPoint.z, graspPoint.y + table.GetComponent<Renderer>().bounds.max.y - 0.0125f, 1.18f + graspPoint.x), rotation);
+				GameObject cube = Instantiate(cubePrefab, UnityUtil.DobotArmToVR(graspPoint) + new Vector3(0,-0.0125f,0), rotation);
 				cube.name = "cube_" + i;
 				cube.transform.localScale = new UnityEngine.Vector3(0.025f, 0.025f, 0.025f);
 				cube.GetComponent<Renderer>().material.color = color;
+				graspPositions[cube.name] = grasp;
 				i++;
 
 			}
 			shouldUpdateVisualization = false;
 			isMessageReceived = false;
+		}
+
+		public MessageTypes.Geometry.Point getGraspPoint(string name)
+		{
+			return graspPositions[name];
 		}
 
 	}
