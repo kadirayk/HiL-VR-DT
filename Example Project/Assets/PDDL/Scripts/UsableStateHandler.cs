71c1118ed632ddd904b82fd481b83d0fa904d96f
kadirayk <kadiray.k@hotmail.com>
2020-07-05 21:09:12 +0200

    usableStateHandler for ui controllers

---
+++ Example Project/Assets/PDDL/Scripts/UsableStateHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UsableStateHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		UserActionRecorder actionRecorder = GameObject.FindObjectOfType<UserActionRecorder>();
+		actionRecorder.RegisterUsable(gameObject);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
